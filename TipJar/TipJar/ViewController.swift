1235d0351e429cf45ffb34019a493970007814eb
Richard Stockdale <richardstockdale@mac.com>
2019-01-16 13:06:39 +0000

    Project Creation

---
+++ TipJar/TipJar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipJar
+//
+//  Created by Richard Stockdale on 16/01/2019.
+//  Copyright © 2019 JunctionSeven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
