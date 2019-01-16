17371b8a51765a9b972184cb2af13461cfa2eec4
Richard Stockdale <richardstockdale@mac.com>
2019-01-16 14:09:05 +0000

    Remove cruft and gitignore

---
+++ TipJar/TipJar/User Interface/TipJarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TipJarViewController.swift
+//  TipJar
+//
+//  Created by Richard Stockdale on 16/01/2019.
+//  Copyright © 2019 JunctionSeven. All rights reserved.
+//
+
+import UIKit
+
+class TipJarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
